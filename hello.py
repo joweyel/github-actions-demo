4db245f56923e4458fd1b1f7da3c972f5cfd4c20
joweyel <falsh@protonmail.com>
2023-08-12 22:18:03 +0000

    adding changes from Azure

--- hello.py
+++ hello.py
@@ -3,13 +3,23 @@ def add(x, y):
 
     return x + y
 
+
 def subtract(x, y):
     """This is an subtract function"""
     return x - y
 
+
 def multiply(x, y):
     """This is an multiplication function"""
     return x * y
+
+
+def division(x, y):
+    """This is an division function"""
+    if y == 0:
+        return None
+    else:
+        return x / y
 
 
 print(add(1, 1))
--- test_hello.py
+++ test_hello.py
@@ -1,11 +1,21 @@
-from hello import add, subtract, multiply
+from hello import add, subtract, multiply, division
 
 
 def test_add():
     assert 2 == add(1, 1)
 
+
 def test_subtract():
     assert 0 == subtract(1, 1)
-    
+
+
 def test_multiply():
     assert 6 == multiply(2, 3)
+
+
+def test_division1():
+    assert 2 == division(6, 3)
+
+
+def test_division2():
+    assert None == division(6, 0)
